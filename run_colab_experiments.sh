3f82cd19ee0f87081da27ff23ffe9cb1402a416c
Bhushan Deshpande <bhushan1729dsp@gmail.com>
2026-05-18 11:42:51 +0530

    updated .sh

--- run_colab_experiments.sh
+++ run_colab_experiments.sh
@@ -2,7 +2,7 @@
 # Colab Run Script for Olfaction-Inspired NER
 # Runs sequentially. (Parallel execution causes CPU/Disk bottlenecks on Colab).
 
-BASE_SAVE_DIR="/content/drive/My Drive/olfaction_inspired_ner"
+BASE_SAVE_DIR="/content/drive/My Drive/olfaction_inspired_ner/no_pretrained_embeddings"
 SEEDS=(42 123 456 789 1011)
 EXPERIMENTS=("baseline" "olfactory" "receptors_only" "no_sparsity" "more_receptors" "more_glomeruli")
 DATASETS=("conll_en" "wikiann_mr" "wikiann_hi" "wikiann_ta" "wikiann_bn" "wikiann_te")
